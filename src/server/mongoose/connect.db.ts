dd2e7ac86bc377190de0576b2f0250a19f3ee76b
Benmrad Bilel <109316958+BenMradB@users.noreply.github.com>
2025-04-25 06:38:10 +0100

    now once user connect with clerk that will fire an even that will be catched by the app and reate that user into my own database so the app data be in sync with clerk

--- src/server/mongoose/connect.db.ts
+++ src/server/mongoose/connect.db.ts
@@ -9,12 +9,14 @@ export const connectToDatabase = async () => {
 
   const URI = process.env.MONGODB_URL.replace(
     "<db_password>",
-    process.env.MONGODB_PASSWORD!
+    process.env.MONGODB_PWD!
   );
+
+  console.log("URI", URI); // Log the URI to check if it's correct
 
   try {
     await mongoose.connect(URI, {
-      dbName: process.env.DATABASE_NAME,
+      dbName: process.env.DATABASE_NAME!,
     });
     isConnected = true;
     console.info("MongoDB Connected");
